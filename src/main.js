09f7d4c76f5c451dbf5133387fcb4b6e3f9862ff
Claudia Reynders <mwl-clau@Claudias-MacBook-Pro.local>
2017-04-12 11:27:33 +0200

    Included components for testing

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
 let React =  require('react');
 let ReactDOM = require('react-dom');
+let AvatarSelector = require('./components/AvatarSelector');
+let LevelCounter = require('./components/LevelCounter');
+let PlayerBadge = require('./components/PlayerBadge');
+
+
+
+// ReactDOM.render(
+//   <AvatarSelector />,
+//   document.getElementById('app')
+// )
+
+// ReactDOM.render(
+//   <LevelCounter levelType="characterLevel"/>,
+//   document.getElementById('app')
+// )
+
+ReactDOM.render (
+    <PlayerBadge />,
+    document.getElementById('app')
+)
