ed74ac058986c729411b7302f14c97bf7b9e4f80
Claudia Reynders <mwl-clau@Claudias-MacBook-Pro.local>
2017-04-12 11:24:44 +0200

    created an avatar object and the data for the avatars

---
+++ src/lib/avatars.js
@@ -0,0 +1,52 @@
+const avatar = {
+  id: 0,
+  name: "",
+  img: ""
+}
+
+const avatarList = [
+  {
+    "id": 1,
+    "name": "Finn",
+    "image": "assets/finn.png"
+  },
+  {
+    "id": 2,
+    "name": "Jake",
+    "image": "assets/jake.png"
+  },
+  {
+    "id": 3,
+    "name": "BMO",
+    "image": "assets/bmo.png"
+  },
+  {
+    "id": 4,
+    "name": "Lady Rainicorn",
+    "image": "assets/lady_rainicorn.png"
+  },
+  {
+    "id": 5,
+    "name": "Princess Bubblegum",
+    "image": "assets/princess_bubblegum.png"
+  },
+  {
+    "id": 6,
+    "name": "Flame princess",
+    "image": "assets/flame_princess.png"
+  },
+  {
+    "id": 7,
+    "name": "Lumpy Space Princess",
+    "image": "assets/lumpy_space_princess.png"
+  },
+  {
+    "id": 8,
+    "name": "Marceline",
+    "image": "assets/marceline.png"
+  }
+];
+
+module.exports = {
+  AvatarList: avatarList
+}
