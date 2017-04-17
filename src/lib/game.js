bcadb4e6288adc9cb5990254e5906442abc8e29b
Claudia Reynders <mwl-clau@Claudias-MacBook-Pro.local>
2017-04-17 11:52:45 +0200

    renamed createPlayer to addPlayerToGame and added updateActivePlayer function

--- src/lib/game.js
+++ src/lib/game.js
@@ -20,7 +20,41 @@ const game = {
       this.currentGame.playerList = previousGame.playerList;
     }
   },
-  createPlayer: function() {
-    this.playerList.push(player.create());
+  addPlayerToGame: function(obj){
+    if(this.playerList.length < 6) {
+      let newPlayer = player.create();
+      Object.assign(newPlayer, obj);
+      this.playerList.push(newPlayer);
+
+      if(this.playerList.length === 6) {
+        return "disabled";
+      }
+    }
+  },
+  updateActivePlayer: function() {
+    console.log("active player: " + this.activePlayer);
+    if(this.activePlayer === null) {
+      this.activePlayer = 1;
+      this.nextPlayer = 2;
+    } else {
+        this.activePlayer = this.nextPlayer;
+        if((this.nextPlayer+1) > this.playerList.length) {
+          this.nextPlayer = 1;
+        } else {
+          this.nextPlayer++;
+        }
+    }
+
+    console.log("active player after: " + this.activePlayer);
+    console.log("next player: " + this.nextPlayer);
   }
 }
+
+
+
+
+module.exports.create = function() {
+  let newGame = Object.assign({}, game);
+  newGame.id = counter+1;
+  return newGame;
+};
