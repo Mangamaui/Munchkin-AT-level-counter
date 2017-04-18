1018ee26a0d21d852d5f84676b3844f5037a2eaa
Claudia Reynders <mwl-clau@Claudias-MacBook-Pro.local>
2017-04-18 16:06:09 +0200

    cleanup main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,6 @@
 let React =  require('react');
 let ReactDOM = require('react-dom');
 let App = require('./components/App');
-let AvatarSelector = require('./components/AvatarSelector');
-//let LevelCounter = require('./components/LevelCounter');
-let CurrentPlayerModal = require('./components/CurrentPlayerModal');
-
-
-
-// ReactDOM.render(
-//   <AvatarSelector />,
-//   document.getElementById('app')
-// )
-
-// ReactDOM.render(
-//   <LevelCounter levelType="characterLevel"/>,
-//   document.getElementById('app')
-// )
 
 ReactDOM.render (
     <App />,
