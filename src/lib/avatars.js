af107c7b0dfc99e08b9e0346dae9b9e9a27e0c47
Claudia Reynders <mwl-clau@claudias-mbp.home>
2017-05-07 13:40:02 +0200

    updated avatar images and added a copy of avatarList to keep track of the available avatars

--- src/lib/avatars.js
+++ src/lib/avatars.js
@@ -8,53 +8,52 @@ const avatarList = [
   {
     "id": 1,
     "name": "Finn",
-    "image": "assets/finn.png"
+    "image": "assets/images/avatars/finn.png"
   },
   {
     "id": 2,
     "name": "Jake",
-    "image": "assets/jake.png"
+    "image": "assets/images/avatars/jake.png"
   },
   {
     "id": 3,
     "name": "BMO",
-    "image": "assets/bmo.png"
+    "image": "assets/images/avatars/bmo.png"
   },
   {
     "id": 4,
     "name": "Lady Rainicorn",
-    "image": "assets/lady_rainicorn.png"
+    "image": "assets/images/avatars/lady_rainicorn.png"
   },
   {
     "id": 5,
     "name": "Princess Bubblegum",
-    "image": "assets/princess_bubblegum.png"
+    "image": "assets/images/avatars/princess_bubblegum.png"
   },
   {
     "id": 6,
     "name": "Flame princess",
-    "image": "assets/flame_princess.png"
+    "image": "assets/images/avatars/flame_princess.png"
   },
   {
     "id": 7,
     "name": "Lumpy Space Princess",
-    "image": "assets/lumpy_space_princess.png"
+    "image": "assets/images/avatars/lumpy_space_princess.png"
   },
   {
     "id": 8,
     "name": "Marceline",
-    "image": "assets/marceline.png"
+    "image": "assets/images/avatars/marceline.png"
   }
 ];
 
 const availableAvatarList = avatarList.slice();
 
 module.exports = {
-  AvatarList: availableAvatarList,
+  AvatarList: avatarList,
+  AvailableAvatarList: availableAvatarList,
   UpdateAvailableAvatars: function(id) {
-    let index = avatarList.findIndex(avatar => avatar.id === id);
-
-    availableAvatarList.splice(index,1);
-    console.log(availableAvatarList);
+    let index = availableAvatarList.findIndex(avatar => avatar.id === id);
+    availableAvatarList.splice(index, 1);
   }
 }
