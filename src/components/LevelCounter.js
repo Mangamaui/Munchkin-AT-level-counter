48e2ffecf9d9014858afa60727f0078853aa2247
Claudia Reynders <mwl-clau@Claudias-MacBook-Pro.local>
2017-05-11 12:39:33 +0200

    added classnames to all elements

--- src/components/LevelCounter.js
+++ src/components/LevelCounter.js
@@ -4,11 +4,11 @@ class LevelCounter extends React.Component {
 
   render() {
     return (
-      <div className="LevelCounter">
-        <h3>{this._setLevelTypeTitle()}</h3>
-        <button className="level_down" onClick={this.clickHandler("decrease").bind(this)}>-</button>
-        <p>{this.props.currentPlayer[this.props.levelType]}</p>
-        <button className="level_up" onClick={this.clickHandler("increase").bind(this)}>+</button>
+      <div className="level_counter">
+        <h3 className="level_counter__label">{this._setLevelTypeTitle()}</h3>
+        <button className="level_down level_counter__button level_counter__button_down icon-minus" onClick={this.clickHandler("decrease").bind(this)}></button>
+        <p className="level_counter__level">{this.props.currentPlayer[this.props.levelType]}</p>
+        <button className="level_up level_counter__button level_cunter__button_up icon-plus" onClick={this.clickHandler("increase").bind(this)}></button>
       </div>
     )
   }
@@ -25,7 +25,7 @@ class LevelCounter extends React.Component {
   _setLevelTypeTitle() {
     let lvlType = this._setToUpperCase();
     let pos = lvlType.indexOf("Level");
-    let word = lvlType.slice(0,pos) + " " + lvlType.slice(pos) + ":";
+    let word = lvlType.slice(0,pos) + " " + lvlType.slice(pos);
     return word;
   }
 
