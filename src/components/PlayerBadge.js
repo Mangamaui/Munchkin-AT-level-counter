f7c493223cf983b277eed5dab235096c2030f0df
Claudia Reynders <mwl-clau@Claudias-MacBook-Pro.local>
2017-05-24 13:10:58 +0200

    added extra classes for styling

--- src/components/PlayerBadge.js
+++ src/components/PlayerBadge.js
@@ -12,10 +12,11 @@ class PlayerBadge extends React.Component {
   render() {
     return (
       <div className="PlayerBadge">
-        <img className="stone_slab" src="assets/images/stone_slab_80.svg" />
+        <img className="stone_slab mobile_slab" src="assets/images/stone_slab_80.svg" />
+        <img className="stone_slab desktop_slab" src="assets/images/stone_slab_300.svg" />
         <img className="PlayerBadge_avatar" src={this.loadAvatar()} />
-        <p className="PlayerBadge_name">{this.props.currentPlayer.name}</p>
-        <p className="PlayerBadge_level">{this.props.currentPlayer.characterLevel}</p>
+        <p className="PlayerBadge_name PlayerBadge_text">{this.props.currentPlayer.name}</p>
+        <p className="PlayerBadge_level PlayerBadge_text">{this.props.currentPlayer.characterLevel}</p>
       </div>
     )
   }
